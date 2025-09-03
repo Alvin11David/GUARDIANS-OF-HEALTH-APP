01c924aa0f0a7c5ebafc422f09a48675f51d6072
Alvin11David <alvin69david@gmail.com>
2025-09-03 04:24:32 +0300

    Finished setting up firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:guardians_of_health/user%20pages/Splash/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
