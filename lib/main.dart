2e005af043e0b49711c1867809d6e7b310134c9a
Larrie-Tau <kasibantelarry@gmail.com>
2025-09-05 00:57:59 +0300

    Line 4: import 'package:guardians_of_health/user pages/dashboards/trends&stats_screen.dart';

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:guardians_of_health/user pages/dashboards/trends&stats_screen.dart';
