b0a665b31838e47e0f0d751d7e71ecd8d748ffac
Larrie-Tau <kasibantelarry@gmail.com>
2025-09-05 01:00:18 +0300

    macos/Flutter/GeneratedPluginRegistrant.swift [Word 30]: FLTFirebaseAuthPlugin.register(with:

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -13,3 +13,4 @@ import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) { 
 FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin")) 
+FLTFirebaseAuthPlugin.register(with: 
