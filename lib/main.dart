043eabb9c12c03d9113716c93f07552c931851f3
Larrie-Tau <kasibantelarry@gmail.com>
2025-09-05 00:57:59 +0300

    Line 6: void main() async {

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,4 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:guardians_of_health/user pages/dashboards/trends&stats_screen.dart';
 
+void main() async {
