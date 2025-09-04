d1f49d2466b85ac769da54d9062b9af579b6fe08
Larrie-Tau <kasibantelarry@gmail.com>
2025-09-05 01:02:37 +0300

    windows/flutter/generated_plugins.cmake [Word 21]: add_subdirectory(flutter/ephemeral/.plugin_symlinks/${plugin}/windows

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -14,3 +14,4 @@ list(APPEND FLUTTER_FFI_PLUGIN_LIST
 set(PLUGIN_BUNDLED_LIBRARIES) 
 
 foreach(plugin ${FLUTTER_PLUGIN_LIST}) 
+add_subdirectory(flutter/ephemeral/.plugin_symlinks/${plugin}/windows 
