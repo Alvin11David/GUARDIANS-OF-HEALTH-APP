3d200f26aab809c3c465d29996b61ac3ca3ab875
Alvin11David <alvin69david@gmail.com>
2025-09-05 20:04:33 +0300

    Added the skip word

--- lib/user pages/intro/onboarding_screen1.dart
+++ lib/user pages/intro/onboarding_screen1.dart
@@ -1,14 +1,65 @@
 import 'package:flutter/material.dart';
 
-class OnboardingScreen1 extends StatelessWidget {
+class OnboardingScreen1 extends StatefulWidget {
   const OnboardingScreen1({super.key});
 
   @override
+  State<OnboardingScreen1> createState() => _OnboardingScreen1State();
+}
+
+class _OnboardingScreen1State extends State<OnboardingScreen1> {
+  bool _isGlowing = false;
+
+  @override
   Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       body: Container(
-        color: Colors.black
-      )
+        color: Colors.black,
+        child: Stack(
+          children: [
+            Positioned(
+              top: screenHeight * 0.04,
+              right: screenWidth * 0.04,
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _isGlowing = true;
+                  });
+                  Future.delayed(const Duration(milliseconds: 200), () {
+                    setState(() {
+                      _isGlowing = false;
+                    });
+                  });
+                },
+                child: AnimatedContainer(
+                  duration: const Duration(milliseconds: 200),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: screenWidth * 0.02,
+                    vertical: screenHeight * 0.01,
+                  ),
+                  decoration: BoxDecoration(
+                    color: _isGlowing ? Colors.white.withOpacity(0.2) : Colors.transparent,
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(screenWidth * 0.04),
+                  ),
+                  child: Text(
+                    'Skip',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: screenWidth * 0.03,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Lufga',
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
