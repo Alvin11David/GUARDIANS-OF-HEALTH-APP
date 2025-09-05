390915161f276b517247d06925e90667632533b3
Alvin11David <alvin69david@gmail.com>
2025-09-05 20:05:34 +0300

    Added the skip word

---
+++ lib/user pages/intro/onboarding_screen2.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class OnboardingScreen2 extends StatefulWidget {
+  const OnboardingScreen2({super.key});
+
+  @override
+  State<OnboardingScreen2> createState() => _OnboardingScreen2State();
+}
+
+class _OnboardingScreen2State extends State<OnboardingScreen2> {
+  bool _isGlowing = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: Container(
+        color: Colors.black,
+        child: Stack(
+          children: [
+            Positioned(
+              top: screenHeight * 0.04,
+              right: screenWidth * 0.04,
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _isGlowing = true;
+                  });
+                  Future.delayed(const Duration(milliseconds: 200), () {
+                    setState(() {
+                      _isGlowing = false;
+                    });
+                  });
+                },
+                child: AnimatedContainer(
+                  duration: const Duration(milliseconds: 200),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: screenWidth * 0.02,
+                    vertical: screenHeight * 0.01,
+                  ),
+                  decoration: BoxDecoration(
+                    color: _isGlowing ? Colors.white.withOpacity(0.2) : Colors.transparent,
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(screenWidth * 0.04),
+                  ),
+                  child: Text(
+                    'Skip',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: screenWidth * 0.03,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Lufga',
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
