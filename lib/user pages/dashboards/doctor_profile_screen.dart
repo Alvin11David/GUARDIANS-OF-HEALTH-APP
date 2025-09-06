eb463faa3718ff06a6d5391538ef65b11629aabf
sekibaalamark <markmajje256@gmail.com>
2025-09-06 22:54:06 +0300

    added the bell notification icon in the top right corner of Dr screen

--- lib/user pages/dashboards/doctor_profile_screen.dart
+++ lib/user pages/dashboards/doctor_profile_screen.dart
@@ -3,37 +3,58 @@ import 'package:flutter/material.dart';
 class DoctorProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final double iconSize = MediaQuery.of(context).size.width * 0.12;
     return Scaffold(
-      body: Container(
-        color: Color(0xFFEFE6E6),
-        child: Stack(
+      backgroundColor: Color(0xFFEFE6E6),
+      body: SafeArea(
+        child: Column(
           children: [
-            Column(
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-              Padding(
-                padding: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.height * 0.02,
-                  left: MediaQuery.of(context).size.width * 0.04,
-                ),
-                child: Container(
-                  width: MediaQuery.of(context).size.width * 0.12,
-                  height: MediaQuery.of(context).size.height * 0.06,
+                Container(
+                  width: iconSize,
+                  height: iconSize,
                   decoration: BoxDecoration(
                     color: Colors.white,
-                    borderRadius: BorderRadius.circular(20), // Fixed border radius of 20
+                    borderRadius: BorderRadius.circular(iconSize / 2),
                   ),
-                  child: Icon(
-                    Icons.chevron_left,
-                    color: Colors.black,
-                    size: MediaQuery.of(context).size.width * 0.06,
+                  child: IconButton(
+                    icon: Icon(Icons.chevron_left, color: Colors.black),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
                   ),
                 ),
-              ),
-            ],
-          ),
-          ]
+                Row(
+                  children: [
+                    Container(
+                      width: iconSize,
+                      height: iconSize,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(iconSize / 2),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.1),
+                            blurRadius: 5,
+                            offset: Offset(0, 2),
+                          ),
+                        ],
+                      ),
+                      child: IconButton(
+                        icon: Icon(Icons.notifications_none, color: Colors.black),
+                        onPressed: () {},
+                      ),
+                    ),
+                    SizedBox(width: 16), // Space for another icon
+                  ],
+                ),
+              ],
+            ),
+            // Placeholder for other content
+          ],
         ),
-        
       ),
     );
   }
