908aaf59174dca07b24e3af0e459d38ef8b7f0e1
sekibaalamark <markmajje256@gmail.com>
2025-09-06 23:39:07 +0300

    added text profile in the dr profile

--- lib/user pages/dashboards/doctor_profile_screen.dart
+++ lib/user pages/dashboards/doctor_profile_screen.dart
@@ -4,10 +4,12 @@ class DoctorProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final double iconSize = MediaQuery.of(context).size.width * 0.12;
+    final double textSize = MediaQuery.of(context).size.height * 0.025; // Reduced text size slightly
     return Scaffold(
       backgroundColor: Color(0xFFEFE6E6),
       body: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -15,6 +17,7 @@ class DoctorProfileScreen extends StatelessWidget {
                 Container(
                   width: iconSize,
                   height: iconSize,
+                  margin: EdgeInsets.only(left: 16),
                   decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.circular(iconSize / 2),
@@ -31,6 +34,7 @@ class DoctorProfileScreen extends StatelessWidget {
                     Container(
                       width: iconSize,
                       height: iconSize,
+                      margin: EdgeInsets.only(right: 16),
                       decoration: BoxDecoration(
                         color: Colors.white,
                         borderRadius: BorderRadius.circular(iconSize / 2),
@@ -47,10 +51,43 @@ class DoctorProfileScreen extends StatelessWidget {
                         onPressed: () {},
                       ),
                     ),
-                    SizedBox(width: 16), // Space for another icon
+                    Container(
+                      width: iconSize,
+                      height: iconSize,
+                      margin: EdgeInsets.only(right: 16),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(iconSize / 2),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.1),
+                            blurRadius: 5,
+                            offset: Offset(0, 2),
+                          ),
+                        ],
+                      ),
+                      child: ClipOval(
+                        child: Image.asset(
+                          'assets/images/DrProfilePic.png',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               ],
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 16, top: 8),
+              child: Text(
+                'PROFILE',
+                style: TextStyle(
+                  fontSize: textSize,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                  letterSpacing: 0.0,
+                ),
+              ),
             ),
             // Placeholder for other content
           ],
