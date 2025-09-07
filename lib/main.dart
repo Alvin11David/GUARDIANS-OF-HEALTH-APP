0c3816884ecb69f2df0a2c47b477e4c5874c15ba
FidelJones <leftoversme@gmail.com>
2025-09-07 21:56:42 +0300

    almost done with forgot password sscreen

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:guardians_of_health/firebase_options.dart';
 import 'package:guardians_of_health/user%20pages/Splash/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
